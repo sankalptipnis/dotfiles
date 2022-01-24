6de0cc32c5849b42e0a35aa33760767be0467e34
Sankalp Tipnis <35152001+sankalptipnis@users.noreply.github.com>
2022-01-24 14:08:53 +0000

    Add conditional cmds to dock configuration script

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,14 +1,25 @@
-#!/local/bin/env bash
+#!/usr/bin/env bash
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Mimestream.app"
+dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
+
+if (ls /Applications | grep -q "Google Keep.app"); then
+    dockutil --no-restart --add "/Applications/Google Keep.app"
+fi
+
+dockutil --no-restart --add "/Applications/Sublime Text.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/System/Applications/App Store.app"
 dockutil --no-restart --add "/System/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Sublime Text.app"
-dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
-dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+
+if [ -d "$HOME/Google Drive" ]; then
+    dockutil --no-restart --add "$HOME/Google Drive" --view grid --display folder --sort name
+fi
+
 dockutil --no-restart --add "/Applications" --view grid --display folder --sort name
 
 killall Dock
