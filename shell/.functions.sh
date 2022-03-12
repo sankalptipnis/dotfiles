00b96f1f52fa3c57e921417d82baf6e0e950d00e
Sankalp Tipnis <35152001+sankalptipnis@users.noreply.github.com>
2022-03-12 10:48:03 +0000

    delete extract function

--- shell/.functions.sh
+++ shell/.functions.sh
@@ -140,32 +140,6 @@ function renametab () {
     echo -ne "\033]0;$@\007"
 }
 
-# Extract archives - use: extract <file>
-function extract() {
-	if [[ -f "$1" ]]; then
-		if command -v unar &>/dev/null; then
-			unar "$1"
-		else
-			case "$1" in
-				*.tar.bz2) tar xjf "$1" ;;
-				*.tar.gz) tar xzf "$1" ;;
-				*.bz2) bunzip2 "$1" ;;
-				*.rar) rar x "$1" ;;
-				*.gz) gunzip "$1" ;;
-				*.tar) tar xf "$1" ;;
-				*.tbz2) tar xjf "$1" ;;
-				*.tgz) tar xzf "$1" ;;
-				*.zip) unzip "$1" ;;
-				*.Z) uncompress "$1" ;;
-				*.7z) 7z x "$1" ;;
-				*) echo "$1 cannot be extracted via extract()" ;;
-			esac
-		fi
-	else
-		echo "$1 is not a valid file"
-	fi
-}
-
 # Use Git’s colored diff when available
 if command -v git &>/dev/null; then
 	function diff() {
