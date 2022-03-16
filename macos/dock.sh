7c4854fb2f73441cd07f6bdb52fc852f85c64357
Sankalp Tipnis <35152001+sankalptipnis@users.noreply.github.com>
2022-03-16 15:19:22 +0000

    sorts dotfiles and drive dirs in dock by kind

--- macos/dock.sh
+++ macos/dock.sh
@@ -10,8 +10,8 @@ dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Sublime Text.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/System/Applications/System Preferences.app"
-[ -d "$DOTFILES_DIR" ] && dockutil --no-restart --add "$DOTFILES_DIR" --view grid --display folder --sort name
-[ -d "$HOME/Drive" ] && dockutil --no-restart --add "$HOME/Drive" --view grid --display folder --sort name
+[ -d "$DOTFILES_DIR" ] && dockutil --no-restart --add "$DOTFILES_DIR" --view grid --display folder --sort kind
+[ -d "$HOME/Drive" ] && dockutil --no-restart --add "$HOME/Drive" --view grid --display folder --sort kind
 dockutil --no-restart --add "/Applications" --view grid --display folder --sort name
 
 killall Dock
