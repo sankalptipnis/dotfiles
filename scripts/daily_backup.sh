19efad0bdab102977044911c835f2a21a011716b
Sankalp Tipnis <35152001+sankalptipnis@users.noreply.github.com>
2022-04-12 21:51:53 +0100

    fix typos in backup scripts

--- scripts/daily_backup.sh
+++ scripts/daily_backup.sh
@@ -8,7 +8,7 @@ fi
 
 PATH="$HOME/dotfiles/bin:$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
-DATE="`date +"%Y-%m-%d"`"
+DATE="$(date +"%Y-%m-%d")"
 LOG_DIR="$HOME/Borg/Logs"
 LOG_NAME="borg.daily-backup.$DATE.log"
 mkdir -p "$LOG_DIR"
@@ -16,4 +16,4 @@ mkdir -p "$LOG_DIR"
 backup create 2>&1 | ts | tee -a "$LOG_DIR"/"$LOG_NAME"
 sleep 3
 backup prune 2>&1 | ts | tee -a "$LOG_DIR"/"$LOG_NAME"
-[[ $(date +%u) == "3" ]] && sleep 3 && backup check | ts | tee -a "$LOG_DIR"/"$LOG_NAME"
+[[ $(date +%u) == "1" ]] && sleep 3 && backup check | ts | tee -a "$LOG_DIR"/"$LOG_NAME"
