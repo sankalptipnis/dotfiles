75d63d78c929479e825030a62f70b80200bb7604
Sankalp Tipnis <35152001+sankalptipnis@users.noreply.github.com>
2023-10-19 18:49:52 +0100

    add haskell binaries to path

--- shell/.path.sh
+++ shell/.path.sh
@@ -12,6 +12,9 @@ for d in "$HOMEBREW_PREFIX"/opt/*/libexec/gnubin; do prepend-path "$d"; done
 # Make own utilities available
 prepend-path "$DOTFILES_DIR/bin"
 
+# Add Haskell related binaries
+prepend-path "$HOME/.ghcup/bin"
+
 # Remove duplicates
 # Source: http://unix.stackexchange.com/a/40755
 PATH=$(echo -n "$PATH" | awk -v RS=: '{ if (!arr[$0]++) {printf("%s%s",!ln++?"":":",$0)}}')
