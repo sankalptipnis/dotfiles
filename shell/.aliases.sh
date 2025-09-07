ce30f0e02f5ad0e189f8c4195750c88e3ff4d4b5
Sankalp Tipnis <35152001+sankalptipnis@users.noreply.github.com>
2025-09-07 13:23:55 +0100

    remove unused alias

--- shell/.aliases.sh
+++ shell/.aliases.sh
@@ -6,7 +6,6 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ~="cd ~" # `cd` is probably faster to type though
-alias -- -="cd -"
 
 # Unix command shortcuts
 alias rr="rm -rf"
